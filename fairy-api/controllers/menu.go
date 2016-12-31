cc816e92fc45f2704b428b19ca565fc37f2b8027
bobkentt <bobkentt@163.com>
2016-12-31 12:02:59 +0800

    add offset & limit

--- fairy-api/controllers/menu.go
+++ fairy-api/controllers/menu.go
@@ -13,6 +13,17 @@ type MenuController struct {
 
 func (this *MenuController) GetMenuList() {
     fmt.Printf("Call GetMenuList\n")
+
+    // Set default offset & limit
+    offset, err:= this.GetInt("offset")
+    if err != nil {
+        offset = 0
+    }
+    limit, err := this.GetInt("limit")
+    if err != nil {
+        limit = 20
+    }
+    
     _, produts, err := storage.GetProduct()
     if err != nil {
         this.errResponse(config.ERR_INTERNAL, err.Error(), 500, nil)    
